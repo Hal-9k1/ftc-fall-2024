09f35d28abcc26b76a207f4eeeedcfd334d570ee
Ziyao Yuan <zyuanboost2026@gmail.com>
2024-10-28 22:34:19 -0700

    Add new imports and left unhelpful comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDriveLayer/TwoWheelDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDriveLayer/TwoWheelDrive.java
@@ -1,10 +1,17 @@
 package org.firstinspires.ftc.teamcode.task;
+
+// More imports that doesn't exist yet except for Units.
+import task.AxialMovementTask;
+import task.TankDriveTask;  //Is there really a tank drive task? or does it mean tank drive layer?
+import task.TurnTask;
+import UnsupportedTaskError;
+import org.firstinspires.ftc.teamcode.Units;
 /**
  * Drive layer for a two-wheel drive robot.
  * The id of the drive motor controller.
  */
 
-public class TwoWheelDrive
+public class TwoWheelDrive implements Layer
 {
     private static String driveKoalaBear = "6_spamandeggs"; //I don't know what this means ;-;, also i'm kinda sleepy, and i kinda 
     private static double ticksPerRot = 888; //remmeber eddy saying that these numbers and some of the improted stuff won't work 
@@ -14,5 +21,9 @@ public class TwoWheelDrive
     
     private Wheel leftWheel;
     private Wheel rightWheel;
+    // I hope initInfo is actually from LayerSetupInfo I can't rember
+    public TwoWheelDrive(LayerSetupInfo initInfo){
+        // some stuff from FTCLib, haven't read it yet.
+    }
     //Imma leave it here and call it for now
 }
