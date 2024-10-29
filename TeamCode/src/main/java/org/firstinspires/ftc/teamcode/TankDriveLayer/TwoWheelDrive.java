0085d0976a0563ec2a130835e4ff97cf88d43d6b
Man Chan <157547523+Atrisnea@users.noreply.github.com>
2024-10-28 21:52:05 -0700

    Added TankDrive interface & TwoWheelDrive base (Maybe), I might've done it wrong. Also I remember Eddy saying something about having to change it from PIEs motor code to FTC, so maybe wrong.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDriveLayer/TwoWheelDrive.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.task;
+/**
+ * Drive layer for a two-wheel drive robot.
+ * The id of the drive motor controller.
+ */
+
+public class TwoWheelDrive
+{
+    private static String driveKoalaBear = "6_spamandeggs"; //I don't know what this means ;-;, also i'm kinda sleepy, and i kinda 
+    private static double ticksPerRot = 888; //remmeber eddy saying that these numbers and some of the improted stuff won't work 
+    private static double wheelRadius = 0.42; //here so maybe i'm doing this for nothing
+    private static double gearRation = 1;
+    private static double wheelSpanRadius = 0.84;
+    
+    private Wheel leftWheel;
+    private Wheel rightWheel;
+    //Imma leave it here and call it for now
+}
