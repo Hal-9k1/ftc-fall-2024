54447d7d5bb50972f4f05f8530a83fa2e59842dd
Eddy Meals <edimea211@gmail.com>
2024-10-30 07:28:38 -0700

    Add QueuedLayer skeleton

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/layer/QueuedLayer.java
@@ -0,0 +1,34 @@
+package org.firstinspires.teamcode.layer;
+
+import java.util.Iterator;
+import java.util.List;
+import org.firstinspires.teamcode.task.Task; // (Remove this comment) doesn't exist yet
+// (Remove this comment) we don't have to import the other stuff because it's in the same package
+
+// (Remove this comment) please write implementations for these methods and a javadoc for the class.
+
+/**
+ * Javadoc goes here. For help writing javadocs: https://www.oracle.com/technical-resources/articles/java/javadoc-tool.html#:~:text=Writing%20Doc%20Comments-,Format%20of%20a%20Doc%20Comment,-A%20doc%20comment
+ */
+public abstract class QueuedLayer implements Layer {
+  /**
+   * Also describe fields, even private ones, with javadocs.
+   */
+  private Iterator<Task> subtaskIter;
+
+  public QueuedLayer(LayerSetupInfo info) {
+    subtaskIter = null;
+  }
+
+  public boolean isTaskDone() {
+
+  }
+
+  public void update() {
+
+  }
+
+  protected void setSubtasks(List<Task> subtasks) {
+
+  }
+}
