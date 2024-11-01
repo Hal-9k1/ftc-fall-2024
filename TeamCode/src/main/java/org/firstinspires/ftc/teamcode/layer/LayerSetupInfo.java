5e5d4bb965131f1a2d34cdec6a156a31776faed7
Eddy Meals <edimea211@gmail.com>
2024-11-01 00:17:04 -0700

    Finish layer translations
    Add docstrings from PiE code as doc comments.
    Fix more syntax errors, make sure everything uses the right types, and
    add package + import statements.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/layer/LayerSetupInfo.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.layer;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import java.util.Consumer;
+import org.firstinspitres.ftc.teamcode.RobotController;
+
+/**
+ * Contains the information needed to initialize a layer.
+ */
+public class LayerSetupInfo {
+    private final HardwareMap hardwareMap;
+    private final RobotController robotController;
+
+    /**
+     * Creates a LayerSetupInfo.
+     * @param hardwareMap the source of peripheral interfaces the layer may use to communicate with
+     * hardware.
+     * @param robotController the RobotController that will run the layer.
+     */
+    public LayerSetupInfo(HardwareMap hardwareMap, RobotController robotController) {
+        this.hardwareMap = hardwareMap;
+        this.robotController = robotController;
+    }
+
+    /**
+     * Returns the HardwareMap.
+     * @return A HardwareMap that can retrieve peripheral interfaces for devices connected to the
+     * robot.
+     */
+    public HardwareMap getHardwareMap() {
+        return this.hardwareMap;
+    }
+    /**
+     * Registers a callback to be called on every update of the owning RobotController.
+     * @param listener The callback to be called.
+     */
+    public void addUpdateListener(Consumer<Boolean> listener) {
+        this.robotController.addUpdateListener(listener);
+    }
+}
