5ebddd8df8c7a34f8d76e5da2759ac0cdb9ce20c
Ziyao Yuan <zyuanboost2026@gmail.com>
2024-11-01 23:44:15 -0700

    Translate some drive.py to Java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDriveLayer/TwoWheelDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDriveLayer/TwoWheelDrive.java
@@ -1,6 +1,6 @@
 package org.firstinspires.ftc.teamcode;
-package org.firstinspires.ftc.teamcode.task;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 // More imports that doesn't exist yet except for Units.
 import org.firstinspires.ftc.teamcode.task.AxialMovementTask;
 import org.firstinspires.ftc.teamcode.task.TankDriveTask;  //Is there really a tank drive task? or does it mean tank drive layer?
@@ -12,31 +12,55 @@ import org.firstinspires.ftc.teamcode.Units;
  * The id of the drive motor controller.
  */
 
-public class TwoWheelDrive implements Layer
-{
-    private static String driveKoalaBear = "6_spamandeggs"; //I don't know what this means ;-;, also i'm kinda sleepy, and i kinda 
-                                                            // Possible answer to the above, I think this may be the motor controller ID.
-    private static double ticksPerRot = 888; //remmeber eddy saying that these numbers and some of the improted stuff won't work 
-    private static double wheelRadius = 0.42; //here so maybe i'm doing this for nothing
+public class TwoWheelDrive implements Layer {
+    private static String driveKoalaBear = "6_spamandeggs"; // Don't know what the Koala Bear equivalent in FTC is.
+    // Some values from below might need to be changed according to the hardware parts.
+    private static double ticksPerRot = 888;
+    private static double wheelRadius = 0.42;
     private static double gearRatio = 1;
     private static double wheelSpanRadius = 0.84;
+    private static double slippingConstant = 1;
     // Wheel is a class from Mechanisms.py, probably translated into Mechanism.java
     private Wheel leftWheel;
     private Wheel rightWheel;
 
+    // New important lore drop from Eddy: Use DcMotor and Servo interfaces from FTC in place of the wrapper classes from actuators.py
+    // Sure hope that Wheel class is implemented in Java when this is done.
     public TwoWheelDrive(LayerSetupInfo initInfo){
-        /*Motor is a class from actuators.py, probably translated into actuators.java, 
-        either that or we're using something else like HardwareMap.DeviceMapping<DcMotor> dcMotor.
-        I don't know what I am talking about I may be going insane. */
-
         // Wheel class has three parameters: motor, radius, and ticks per rotation.
-        // Motor class has three parameters: robot, ControllerID, and motor.
-        this.leftWheel = new Wheel(new Motor(initInfo.getRobot(), driveKoalaBear, "a")
-                .setInvert(false)
-                .setPID(null, null, null), // Not sure if we're supposed to use our own, FTCLib might have their own class for this.
+        this.leftWheel = new Wheel(hardwareMap.get(DcMotor.class, "leftdrivemotor"),
             wheelRadius,
-            ticksPerRot
-        );
-        // More work to do...
+            ticksPerRot);
+        this.rightWheel = new Wheel(hardwareMap.get(DcMotor.class, "rightdrivemotor"),
+            wheelRadius,
+            ticksPerRot);
+        
+        // Not sure what modifiers those four variables below need. Have to implement later.
+        double leftStartPos = 0;
+        double rightStartPos = 0;
+
+        double leftGoalDelta = 0;
+        double rightGoalDelta = 0;
+    }
+
+    public boolean isTaskDone(){
+        boolean leftDone = ((this.leftWheel.getDistance() - this.leftStartPos < 0)
+            == (this.leftGoalDelta < 0)) || this.leftGoalDelta == 0;
+        boolean rightDone = ((this.rightWheel.getDistance() - this.rightStartPos< 0)
+            == (this.rightGoalDelta < 0)) || this.rightGoalDelta == 0;
+        boolean done = leftDone && rightDone;
+        if (done) {
+            this.leftWheel.setVelocity(0);
+            this.rightWheel.setVelocity(0);
+            return done;
+        }
+    }
+
+    public update(){
+        // Adaptive velocity controller goes here.
+    }
+
+    public acceptTask(Task task){
+        // Gonna fall asleep will do this tomorrow morning
     }
 }
