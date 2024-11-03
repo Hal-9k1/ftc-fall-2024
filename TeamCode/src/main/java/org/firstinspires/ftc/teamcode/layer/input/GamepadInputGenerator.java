bcc5fbe139926ca312db71cfd7e001caf9b5ff0d
Eddy Meals <edimea211@gmail.com>
2024-11-02 18:13:26 -0700

    Support two gamepads

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/layer/input/GamepadInputGenerator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/layer/input/GamepadInputGenerator.java
@@ -7,30 +7,56 @@ import org.firstinspires.ftc.teamcode.task.Task;
 import org.firstinspires.ftc.teamcode.task.UnsupportedTaskException;
 
 public class GamepadInputGenerator extends InputGenerator {
-    private Gamepad gamepad;
+    private Gamepad gamepad0;
+    private Gamepad gamepad1;
 
     public void setup(LayerSetupInfo setupInfo) {
-        // TODO: add gamepad property to LayerSetupInfo and shove it in this.gamepad
+        gamepad0 = setupInfo.gamepad0;
+        gamepad1 = setupInfo.gamepad1;
+        if (gamepad0 == null && gamepad1 == null) {
+            throw new IllegalArgumentException("At least one gamepad must be connected to the "
+                + "robot.");
+        }
     }
 
     public Task update() {
         return new GamepadInputTask(
-            gamepad.left_stick_x,
-            gamepad.left_stick_y,
-            gamepad.left_bumper,
-            gamepad.left_trigger,
-            gamepad.right_stick_x,
-            gamepad.right_stick_y,
-            gamepad.right_bumper,
-            gamepad.right_trigger,
-            gamepad.dpad_up,
-            gamepad.dpad_right,
-            gamepad.dpad_down,
-            gamepad.dpad_left,
-            gamepad.a,
-            gamepad.b,
-            gamepad.x,
-            gamepad.y
+            gamepad0 == null ? null : new GamepadInputTask.GamepadInput(
+                gamepad0.left_stick_x,
+                gamepad0.left_stick_y,
+                gamepad0.left_bumper,
+                gamepad0.left_trigger,
+                gamepad0.right_stick_x,
+                gamepad0.right_stick_y,
+                gamepad0.right_bumper,
+                gamepad0.right_trigger,
+                gamepad0.dpad_up,
+                gamepad0.dpad_right,
+                gamepad0.dpad_down,
+                gamepad0.dpad_left,
+                gamepad0.a,
+                gamepad0.b,
+                gamepad0.x,
+                gamepad0.y
+            ),
+            gamepad1 == null ? null : new GamepadInputTask.GamepadInput(
+                gamepad1.left_stick_x,
+                gamepad1.left_stick_y,
+                gamepad1.left_bumper,
+                gamepad1.left_trigger,
+                gamepad1.right_stick_x,
+                gamepad1.right_stick_y,
+                gamepad1.right_bumper,
+                gamepad1.right_trigger,
+                gamepad1.dpad_up,
+                gamepad1.dpad_right,
+                gamepad1.dpad_down,
+                gamepad1.dpad_left,
+                gamepad1.a,
+                gamepad1.b,
+                gamepad1.x,
+                gamepad1.y
+            )
         );
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/task/GamepadInputTask.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/task/GamepadInputTask.java
@@ -1,47 +1,115 @@
 package org.firstinspires.ftc.teamcode.task;
 
 public class GamepadInputTask implements Task {
+    /**
+     * Carries information about the axes of a joystick on a gamepad.
+     */
     public static class Joystick {
+        /**
+         * The horizontal axis of the joystick.
+         */
         public final float x;
+        /**
+         * The vertical axis of the joystick.
+         */
         public final float y;
+
         private Joystick(float x, float y) {
             this.x = x;
             this.y = y;
         }
     }
+
+    /**
+     * Carries information about a the pair of joysticks on a gamepad.
+     */
     public static class Joysticks {
+        /**
+         * The left joystick.
+         */
         public final Joystick left;
+        /**
+         * The right joystick.
+         */
         public final Joystick right;
+
         private Joysticks(Joystick left, Joystick right) {
             this.left = left;
             this.right = right;
         }
     }
+
+    /**
+     * Carries information about a pair of symmetrical buttons on a gamepad, such as the triggers or
+     * bumpers.
+     */
     public static class ButtonPair {
+        /**
+         * Whether the left button of the pair is pressed.
+         */
         public final boolean left;
+        /**
+         * Whether the right button of the pair is pressed.
+         */
         public final boolean right;
+
         private ButtonPair(boolean left, boolean right) {
             this.left = left;
             this.right = right;
         }
     }
-    public static class DirectionPad {
+
+    /**
+     * Caries information about a gamepad's directional pad (four buttons arranged to indicate
+     * movement along x and y axes).
+     */
+    public static class DirectionalPad {
+        /**
+         * Whether the up button of the dpad is pressed.
+         */
         public final boolean up;
+        /**
+         * Whether the right button of the dpad is pressed.
+         */
         public final boolean right;
+        /**
+         * Whether the down button of the dpad is pressed.
+         */
         public final boolean down;
+        /**
+         * Whether the left button of the dpad is pressed.
+         */
         public final boolean left;
-        private DirectionPad(boolean up, boolean right, boolean down, boolean left) {
+
+        private DirectionalPad(boolean up, boolean right, boolean down, boolean left) {
             this.up = up;
             this.right = right;
             this.down = down;
             this.left = left;
         }
     }
+
+    /**
+     * Carries information about miscellanous buttons on a gamepad.
+     */
     public static class Buttons {
+        /**
+         * Whether the A gamepad button is pressed.
+         */
         public final boolean a;
+        /**
+         * Whether the B gamepad button is pressed.
+         */
         public final boolean b;
+        /**
+         * Whether the X gamepad button is pressed.
+         */
         public final boolean x;
+        /**
+         * Whether the Y gamepad button is pressed.
+         */
         public final boolean y;
+
         private Buttons(boolean a, boolean b, boolean x, boolean y) {
             this.a = a;
             this.b = b;
@@ -50,39 +118,81 @@ public class GamepadInputTask implements Task {
         }
     }
 
-    private static final float TRIGGER_MIN = 0.3f;
+    /**
+     * Carries information about a single connected gamepad.
+     */
+    public static class GamepadInput {
+        /**
+         * The pair of joysticks on the gamepad.
+         */
+        public final Joysticks joysticks;
+        /**
+         * The pair of bumpers on the gamepad.
+         */
+        public final ButtonPair bumpers;
+        /**
+         * The pair of triggers on the gamepad.
+         */
+        public final ButtonPair triggers;
+        /**
+         * The directional pad on the gamepad.
+         */
+        public final DirectionalPad dpad;
+        /**
+         * The miscellanious buttons on the gamepad.
+         */
+        public final Buttons buttons;
 
-    public final Joysticks joysticks;
-    public final ButtonPair bumpers;
-    public final ButtonPair triggers;
-    public final DirectionPad dpad;
-    public final Buttons buttons;
+        private static final float TRIGGER_MIN = 0.3f;
 
-    public GamepadInputTask(
-        float joystickLeftX,
-        float joystickLeftY,
-        boolean bumperLeft,
-        float triggerLeft,
-        float joystickRightX,
-        float joystickRightY,
-        boolean bumperRight,
-        float triggerRight, 
-        boolean dpadUp,
-        boolean dpadRight,
-        boolean dpadDown,
-        boolean dpadLeft,
-        boolean buttonA,
-        boolean buttonB,
-        boolean buttonX,
-        boolean buttonY
-    ) {
-        joysticks = new Joysticks(
-            new Joystick(joystickLeftX, joystickLeftY),
-            new Joystick(joystickRightX, joystickRightY)
-        );
-        bumpers = new ButtonPair(bumperLeft, bumperRight);
-        triggers = new ButtonPair(triggerLeft >= TRIGGER_MIN, triggerRight >= TRIGGER_MIN);
-        dpad = new DirectionPad(dpadUp, dpadRight, dpadDown, dpadLeft);
-        buttons = new Buttons(buttonA, buttonB, buttonX, buttonY);
+        public GamepadInput(
+            float joystickLeftX,
+            float joystickLeftY,
+            boolean bumperLeft,
+            float triggerLeft,
+            float joystickRightX,
+            float joystickRightY,
+            boolean bumperRight,
+            float triggerRight,
+            boolean dpadUp,
+            boolean dpadRight,
+            boolean dpadDown,
+            boolean dpadLeft,
+            boolean buttonA,
+            boolean buttonB,
+            boolean buttonX,
+            boolean buttonY
+        ) {
+            joysticks = new Joysticks(
+                new Joystick(joystickLeftX, joystickLeftY),
+                new Joystick(joystickRightX, joystickRightY)
+            );
+            bumpers = new ButtonPair(bumperLeft, bumperRight);
+            triggers = new ButtonPair(triggerLeft >= TRIGGER_MIN, triggerRight >= TRIGGER_MIN);
+            dpad = new DirectionalPad(dpadUp, dpadRight, dpadDown, dpadLeft);
+            buttons = new Buttons(buttonA, buttonB, buttonX, buttonY);
+        }
+    }
+
+
+    /**
+     * Input captured from the gamepad connected to the first slot, or null if none is connected.
+     */
+    public final GamepadInput gamepad0;
+    /**
+     * Input captured from the gamepad connected to the second slot, or null if none is connected.
+     */
+    public final GamepadInput gamepad1;
+
+    /**
+     * Constructs a GamepadInputTask with given info about gamepad inputs.
+     * @param gamepad0 Input captured from the gamepad connected to the first slot, or null if none
+     * is connected.
+     * @param gamepad1 Input captured from the gamepad connected to the second slot, or null if none
+     * is connected.
+     */
+    public GamepadInputTask(GamepadInput gamepad0, GamepadInput gamepad1) {
+        gamepad0 = gamepad0;
+        gamepad1 = gamepad1;
     }
 }
