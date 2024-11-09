4621c8e2fb7332b09ab7daaccfe1d162b69e282b
Eddy Meals <edimea211@gmail.com>
2024-11-09 02:12:39 -0800

    Add LiftTask

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/task/LiftTask.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.task;
+
+/**
+ * Raises or lowers the lift.
+ */
+public class LiftTask implements Task {
+    /**
+     * Whether the lift should be raised.
+     */
+    public boolean raise;
+    /**
+     * Whether the lift should be lowered.
+     */
+    public boolean lower;
+
+    /**
+     * Constructs a LiftTask.
+     * @param raise - whether the lift should be raised.
+     * @param lower - whether the lift should be lowered.
+     */
+    public LiftTask(boolean raise, boolean lower) {
+        this.raise = raise;
+        this.lower = lower;
+        if (raise && lower) {
+            throw new InvalidArgumentException("Cannot simultaneously raise and lower the lift.");
+        }
+    }
+}
