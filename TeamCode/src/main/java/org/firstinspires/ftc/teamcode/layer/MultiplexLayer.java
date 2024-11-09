7627509faa9dfb27f7984a9dd95e4a8f32b84f4a
Eddy Meals <edimea211@gmail.com>
2024-11-09 01:59:24 -0800

    Start MultiplexLayer which might be abandoned

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/layer/MultiplexLayer.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.layer;
+
+import java.util.List;
+
+import org.firstinspires.ftc.teamcode.task.Task;
+import org.firstinspires.ftc.teamcode.task.UnsupportedTaskException;
+
+public class MultiplexLayer implements Layer {
+    private final List<Layer> layers;
+
+    public MultiplexLayer(List<Layer> layers) {
+        this.layers = layers;
+    }
+
+    @Override
+    public void setup(LayerSetupInfo setupInfo) {
+        for (Layer layer : layers) {
+            layer.setup(setupInfo);
+        }
+    }
+
+    @Override
+    public Task update() {
+        // ??? How do we know which layer to ask for a new subtask from
+    }
+
+    @Override
+    public boolean isTaskDone() {
+        return layers.stream().anyMatch(Layer::isTaskDone);
+    }
+
+    @Override
+    public void acceptTask(Task task) {
+        boolean anyAccepted = layers.stream().anyMatch((layer) -> {
+            try {
+                layer.acceptTask(task);
+            } catch (UnsupportedTaskException e) {
+                return false;
+            }
+            return true;
+        });
+        if (!anyAccepted) {
+            // Should list component layers, not say MultiplexLayer
+            throw new UnsupportedTaskException(this, layer);
+        }
+    }
+}
