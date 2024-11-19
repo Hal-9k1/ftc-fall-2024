99d072fcb5ec5671778621e4399b4801e549fd5e
Eddy Meals <edimea211@gmail.com>
2024-11-18 21:39:46 -0800

    Add WinLayer and WinTask
    Add WinTask, the top level task accepted by strategy layers. This allows
    strategy QueuedLayers to call setSubtasks in their acceptTask methods
    instead of the setup method, which was causing some kind of first-task
    bug during quals 1 that I can't remember.
    Add WinLayer, whose sole purpose is to emit a WinTask and then mark
    itself as completed.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/layer/strategy/PathlessStrategy.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/layer/strategy/PathlessStrategy.java
@@ -6,6 +6,7 @@ import org.firstinspires.ftc.teamcode.layer.LayerSetupInfo;
 import org.firstinspires.ftc.teamcode.task.Task;
 import org.firstinspires.ftc.teamcode.task.LinearMovementTask;
 import org.firstinspires.ftc.teamcode.task.TurnTask;
+import org.firstinspires.ftc.teamcode.task.WinTask;
 import org.firstinspires.ftc.teamcode.task.UnsupportedTaskException;
 
 import java.util.Arrays;
@@ -18,35 +19,37 @@ import java.util.Arrays;
  */
 public class PathlessStrategy extends QueuedLayer {
     @Override
-    public void setup(LayerSetupInfo setupInfo) {
-        double firstRushDist = 2.1;
-        double rushDist = 2.0;
-        double strafeDist = 0.25;
-        double firstShortStop = 0.2;
-        double firstTurnAngle = 0.125;
-        double firstShoveDist = 0.4;
-        setSubtasks(Arrays.asList(
-            // Spike closest to submersible
-            new LinearMovementTask(Units.convert(firstRushDist, Units.Distance.TILE, Units.Distance.M), 0),
-            new LinearMovementTask(0, Units.convert(-strafeDist, Units.Distance.TILE, Units.Distance.M)),
-            new LinearMovementTask(Units.convert(-rushDist + firstShortStop, Units.Distance.TILE, Units.Distance.M), 0),
-            new TurnTask(Units.convert(-firstTurnAngle, Units.Angle.REV, Units.Angle.RAD)),
-            new LinearMovementTask(Units.convert(-firstShoveDist, Units.Distance.TILE, Units.Distance.M), 0),
-            new LinearMovementTask(Units.convert(-firstShoveDist, Units.Distance.TILE, Units.Distance.M), 0),
-            new TurnTask(Units.convert(firstTurnAngle, Units.Angle.REV, Units.Angle.RAD)),
-            // Second closest
-            new LinearMovementTask(Units.convert(rushDist - firstShortStop, Units.Distance.TILE, Units.Distance.M), 0),
-            new LinearMovementTask(0, Units.convert(-strafeDist, Units.Distance.TILE, Units.Distance.M)),
-            new LinearMovementTask(Units.convert(-rushDist, Units.Distance.TILE, Units.Distance.M), 0),
-            // Third closest
-            new LinearMovementTask(Units.convert(rushDist, Units.Distance.TILE, Units.Distance.M), 0),
-            new LinearMovementTask(0, Units.convert(-strafeDist, Units.Distance.TILE, Units.Distance.M)),
-            new LinearMovementTask(Units.convert(-rushDist, Units.Distance.TILE, Units.Distance.M), 0),
-        ));
-    }
+    public void setup(LayerSetupInfo setupInfo) { }
 
     @Override
     public void acceptTask(Task task) {
-        throw new UnsupportedTaskException(this, task);
+        if (task instanceof WinTask) {
+            double firstRushDist = 2.1;
+            double rushDist = 2.0;
+            double strafeDist = 0.25;
+            double firstShortStop = 0.2;
+            double firstTurnAngle = 0.125;
+            double firstShoveDist = 0.4;
+            setSubtasks(Arrays.asList(
+                // Spike closest to submersible
+                new LinearMovementTask(Units.convert(firstRushDist, Units.Distance.TILE, Units.Distance.M), 0),
+                new LinearMovementTask(0, Units.convert(-strafeDist, Units.Distance.TILE, Units.Distance.M)),
+                new LinearMovementTask(Units.convert(-rushDist + firstShortStop, Units.Distance.TILE, Units.Distance.M), 0),
+                new TurnTask(Units.convert(-firstTurnAngle, Units.Angle.REV, Units.Angle.RAD)),
+                new LinearMovementTask(Units.convert(-firstShoveDist, Units.Distance.TILE, Units.Distance.M), 0),
+                new LinearMovementTask(Units.convert(-firstShoveDist, Units.Distance.TILE, Units.Distance.M), 0),
+                new TurnTask(Units.convert(firstTurnAngle, Units.Angle.REV, Units.Angle.RAD)),
+                // Second closest
+                new LinearMovementTask(Units.convert(rushDist - firstShortStop, Units.Distance.TILE, Units.Distance.M), 0),
+                new LinearMovementTask(0, Units.convert(-strafeDist, Units.Distance.TILE, Units.Distance.M)),
+                new LinearMovementTask(Units.convert(-rushDist, Units.Distance.TILE, Units.Distance.M), 0),
+                // Third closest
+                new LinearMovementTask(Units.convert(rushDist, Units.Distance.TILE, Units.Distance.M), 0),
+                new LinearMovementTask(0, Units.convert(-strafeDist, Units.Distance.TILE, Units.Distance.M)),
+                new LinearMovementTask(Units.convert(-rushDist, Units.Distance.TILE, Units.Distance.M), 0)
+            ));
+        } else {
+            throw new UnsupportedTaskException(this, task);
+        }
     }
 }
