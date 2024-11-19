ac5000a26cb3017f350d9b38750eaf09fd588deb
Eddy Meals <edimea211@gmail.com>
2024-11-18 21:23:24 -0800

    Add PathlessStrategy and tile unit
    Add Units.Distance.TILE to represent a tile-width on the field (24
    inches).
    Fix indentation in Units.java.
    Add PathlessStrategy which emits AxialMovementTasks and TurnTasks
    directly instead of relying on a pathfinder. This strategy scores the
    alliance-neutral preset samples on our side of the field.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Units.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Units.java
@@ -6,40 +6,38 @@ import java.util.HashMap;
 import java.util.Set;
 import java.util.Map;
 
-public class Units
-{
+public class Units {
     private static double pi = Math.PI;
     public static enum Distance {
-      MM(1000),
-      CM(100),
-      IN(100 / 2.54),
-      FT(100 / 2.54 / 12),
-      M(1);
+        MM(1000),
+        CM(100),
+        IN(100 / 2.54),
+        TILE(100 / 2.54 * 24),
+        FT(100 / 2.54 / 12),
+        M(1);
 
-      private final double unitsPerMeter;
+        private final double unitsPerMeter;
 
-      Distance(double unitsPerMeter) {
-        this.unitsPerMeter = unitsPerMeter;
-      }
+        Distance(double unitsPerMeter) {
+            this.unitsPerMeter = unitsPerMeter;
+        }
     }
     public static enum Angle {
-      DEG(180 / Math.PI),
-      REV(1 / (2 * Math.PI)),
-      RAD(1);
+        DEG(180 / Math.PI),
+        REV(1 / (2 * Math.PI)),
+        RAD(1);
 
-      private final double unitsPerRadian;
+        private final double unitsPerRadian;
 
-      Angle(double unitsPerRadian) {
-        this.unitsPerRadian = unitsPerRadian;
-      }
+        Angle(double unitsPerRadian) {
+            this.unitsPerRadian = unitsPerRadian;
+        }
     }
 
-    public static double convert(double value, Distance fromUnit, Distance toUnit)
-    {
+    public static double convert(double value, Distance fromUnit, Distance toUnit) {
         return value * toUnit.unitsPerMeter / fromUnit.unitsPerMeter;
     }
-    public static double convert(double value, Angle fromUnit, Angle toUnit)
-    {
+    public static double convert(double value, Angle fromUnit, Angle toUnit) {
         return value * toUnit.unitsPerRadian / fromUnit.unitsPerRadian;
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/layer/strategy/PathlessStrategy.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.layer.strategy;
+
+import org.firstinspires.ftc.teamcode.Units;
+import org.firstinspires.ftc.teamcode.layer.QueuedLayer;
+import org.firstinspires.ftc.teamcode.layer.LayerSetupInfo;
+import org.firstinspires.ftc.teamcode.task.Task;
+import org.firstinspires.ftc.teamcode.task.LinearMovementTask;
+import org.firstinspires.ftc.teamcode.task.TurnTask;
+import org.firstinspires.ftc.teamcode.task.UnsupportedTaskException;
+
+import java.util.Arrays;
+
+public class PathlessStrategy extends QueuedLayer {
+    @Override
+    public void setup(LayerSetupInfo setupInfo) {
+        double firstRushDist = 2.1;
+        double rushDist = 2.0;
+        double strafeDist = 0.25;
+        double firstShortStop = 0.2;
+        double firstTurnAngle = 0.125;
+        double firstShoveDist = 0.4;
+        setSubtasks(Arrays.asList(
+            // Spike closest to submersible
+            new LinearMovementTask(Units.convert(firstRushDist, Units.Distance.TILE, Units.Distance.M), 0),
+            new LinearMovementTask(0, Units.convert(-strafeDist, Units.Distance.TILE, Units.Distance.M)),
+            new LinearMovementTask(Units.convert(-rushDist + firstShortStop, Units.Distance.TILE, Units.Distance.M), 0),
+            new TurnTask(Units.convert(-firstTurnAngle, Units.Angle.REV, Units.Angle.RAD)),
+            new LinearMovementTask(Units.convert(-firstShoveDist, Units.Distance.TILE, Units.Distance.M), 0),
+            new LinearMovementTask(Units.convert(-firstShoveDist, Units.Distance.TILE, Units.Distance.M), 0),
+            new TurnTask(Units.convert(firstTurnAngle, Units.Angle.REV, Units.Angle.RAD)),
+            // Second closest
+            new LinearMovementTask(Units.convert(rushDist - firstShortStop, Units.Distance.TILE, Units.Distance.M), 0),
+            new LinearMovementTask(0, Units.convert(-strafeDist, Units.Distance.TILE, Units.Distance.M)),
+            new LinearMovementTask(Units.convert(-rushDist, Units.Distance.TILE, Units.Distance.M), 0),
+            // Third closest
+            new LinearMovementTask(Units.convert(rushDist, Units.Distance.TILE, Units.Distance.M), 0),
+            new LinearMovementTask(0, Units.convert(-strafeDist, Units.Distance.TILE, Units.Distance.M)),
+            new LinearMovementTask(Units.convert(-rushDist, Units.Distance.TILE, Units.Distance.M), 0),
+        ));
+    }
+
+    @Override
+    public void acceptTask(Task task) {
+        throw new UnsupportedTaskException(this, task);
+    }
+}
