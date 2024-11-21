4074e1ecf9e535513948d8330681bd7f4998d33a
Ziyao Yuan <zyuanboost2026@gmail.com>
2024-11-20 23:20:56 -0800

    Made the right changes to LiftTask.java
    Accidentally made the wrong changes to LiftTask, hopefully it is fixed now.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/task/LiftTask.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/task/LiftTask.java
@@ -1,45 +1,30 @@
 package org.firstinspires.ftc.teamcode.task;
 
-<<<<<<< HEAD
-public class LiftTask{
-    /**
-     * @param swing is the rotation of the lift/arm in radians.
-     * @param raise is a boolean to determine if the arm is up.
-     * @param lower is a boolean to determine if the arm is down.
-     * 
-     * Raise and lower cannot both be true at the same time.
-     */
-    // There is probably a better way to name raise or lower so that it makes more intuitive sense.
-    public final double swing;
-    public final boolean raise;
-    public final boolean lower;
-
-    public LiftTask(double swing, boolean raise, boolean lower){
-        this.swing = swing;
-        this.raise = raise;
-        this.lower = lower;
-    }
-}
-=======
 /**
  * Raises or lowers the lift.
  */
 public class LiftTask implements Task {
     /**
+     * The rotation of the arm in radians.
+     */
+    public final double swing;
+    /**
      * Whether the lift should be raised.
      */
-    public boolean raise;
+    public final boolean raise;
     /**
      * Whether the lift should be lowered.
      */
-    public boolean lower;
+    public final boolean lower;
 
     /**
      * Constructs a LiftTask.
+     * @param swing - the rotation of the arm in radians.
      * @param raise - whether the lift should be raised.
      * @param lower - whether the lift should be lowered.
      */
-    public LiftTask(boolean raise, boolean lower) {
+    public LiftTask(double swing, boolean raise, boolean lower) {
+        this.swing = swing;
         this.raise = raise;
         this.lower = lower;
         if (raise && lower) {
@@ -47,4 +32,3 @@ public class LiftTask implements Task {
         }
     }
 }
->>>>>>> origin/main
