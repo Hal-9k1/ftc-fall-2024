3e3c76a3b8ab997501399ba957a1c3f7869a4ab2
Ziyao Yuan <zyuanboost2026@gmail.com>
2024-11-20 23:28:34 -0800

    Made LiftTeleopTask implement Task
    Forgor to do it earlier.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/task/LiftTeleopTask.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/task/LiftTeleopTask.java
@@ -1,16 +1,17 @@
 package org.firstinspires.ftc.teamcode.task;
 
-public class LiftTeleopTask{
-    /**
-     * @param swing is the rotation of the arm in radians.
-     * 
-     */
+public class LiftTeleopTask implements Task {
 
      // Unsure of what unit extend is supposed to be, is it linear distance the arm extends? or is it something to do with the motor rotations that make
      // the arm extend? Need to add comment later.
     public final double swing;
     public final double extend;
-    public LiftTeleopTask(double swing, double extend){
+    
+    /**
+     * @param swing - the rotation of the arm in radians.
+     * 
+     */
+    public LiftTeleopTask(double swing, double extend) {
         this.swing = swing;
         this.extend = extend;
     }
