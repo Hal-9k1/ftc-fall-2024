8912b5a1a860fc1fb70a54e5552f18878aa18724
Eddy Meals <edimea211@gmail.com>
2024-12-07 19:22:38 +0000

    Add DriveTower opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/DriveTowerTeleop.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.opmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.layer.Layer;
+import org.firstinspires.ftc.teamcode.layer.drive.MecanumDrive;
+import org.firstinspires.ftc.teamcode.layer.input.GamepadInputGenerator;
+import org.firstinspires.ftc.teamcode.layer.input.mapping.JoystickHoloDriveMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@TeleOp(name="Drive Tower")
+public class DriveTowerTeleop extends LayerOpMode {
+    @Override
+    protected List<Layer> getLayers() {
+        return Arrays.asList(
+            new MultiplexLayer(Arrays.asList(
+                new MecanumDrive(),
+                new TowerLayer()
+            )),
+            new MultiplexLayer(Arrays.asList(
+                new JoystickHoloDriveMapping(),
+                new DpadTowerMapping(),
+                new TriggerIntakeMapping()
+            )),
+            new GamepadInputGenerator()
+        );
+    }
+}
