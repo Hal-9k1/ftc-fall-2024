f5a12d9e5140e568efa87524b1e43c45d836108b
Eddy Meals <edimea211@gmail.com>
2024-12-09 03:55:27 -0800

    Partly write layer.input.mapping package-info

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/layer/input/mapping/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Layers that process raw input tasks and translate them into robot commands.
+ * These layers are best compared to keybinds in video games. They dictate what controls send what
+ * tasks in teleop mode.
+ * <p>
+ * All input mappings are {@link org.firstinspires.ftc.teamcode.layer.FunctionLayer}s because
+ */
+package org.firstinspires.ftc.teamcode.layer.input.mapping;
