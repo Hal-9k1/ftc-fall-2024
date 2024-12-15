a750d0400a21a7e0c0271d2ba55e82d62b7126cf
Ziyao Yuan <zyuanboost2026@gmail.com>
2024-12-15 14:24:35 -0800

    Add TowerForearmTask and TowerTask
    Wrong filepath earlier so these didn't get added.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/task/TowerTask.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/task/TowerTask.java
@@ -1,5 +1,28 @@
 package org.firstinspires.ftc.teamcode.task;
 
+/**
+ * Raises or lowers the arm in an arc.
+ */
 public class TowerTask implements Task {
+    /**
+     * Whether the tower should be fully lifted in an arc.
+     */
+    public boolean raise;
+    /**
+     * Whether the tower should be fully lowered in an arc.
+     */
+    public boolean lower;
 
+    /**
+     * Constructor for TowerTask
+     * @param raise - Whether the tower should be fully lifted in an arc.
+     * @param lower - Whether the tower should be fully lowered in an arc.
+     */
+    public TowerTask(boolean raise, boolean lower) {
+        this.raise = raise;
+        this.lower = lower;
+        if (raise && lower) {
+            throw new IllegalArgumentException("The arm can't be both raised and lowered");
+        }
+    }
 }
