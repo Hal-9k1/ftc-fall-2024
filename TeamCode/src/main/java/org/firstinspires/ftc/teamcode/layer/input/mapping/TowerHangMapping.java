28151379adaecab85f4ffbb3c7be42e0223bc820
Ziyao Yuan <zyuanboost2026@gmail.com>
2024-12-20 12:13:46 -0800

    Add TowerHangMapping
    TowerHangMapping uses gamepad0 button y and a.
    y for hang, a for unhang.
    Maybe need to modify TowerHangTask or create TowerHangTeleopTask to take a double parameter instead of boolean.
    Use checkstyle to fix format issues.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/layer/input/mapping/TowerHangMapping.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.layer.input.mapping;
+
+import org.firstinspires.ftc.teamcode.layer.AbstractFunctionLayer;
+import org.firstinspires.ftc.teamcode.layer.LayerSetupInfo;
+import org.firstinspires.ftc.teamcode.task.GamepadInputTask;
+import org.firstinspires.ftc.teamcode.task.TowerHangTask;  // Either change this for TowerHangTeleopTask or change TowerHangTask to work with teleop.
+import org.firstinspires.ftc.teamcode.task.UnsupportedTaskException;
+
+/**
+ * Button mappings for hanging the robot using tower.
+ */
+public final class TowerHangMapping implements AbstractFunctionLayer {
+    /**
+     * Constructor for TowerHangMapping.
+     */
+    public TowerHangMapping() { }
+
+    @Override
+    public void setup(LayerSetupInfo setupInfo) { }
+
+    @Override
+    public Task map(Task task) {
+        if (task instanceof GamepadInputTask) {
+            GamepadInputTask castedTask = (GamepadInputTask) task;
+            boolean hang = castedTask.gamepad0.buttons.y;
+            boolean unhang = castedTask.gamepad0.buttons.a;
+            return new TowerHangTask((hang ? 1 : 0) - (unhang ? 1 : 0));
+        } else {
+            throw new UnsupportedTaskException(this, task);
+        }
+    }
+}
