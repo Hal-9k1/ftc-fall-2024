e43ec4fd9ba21b67a6aa4bdb6057e6b247b76c86
Ziyao Yuan <zyuanboost2026@gmail.com>
2024-12-20 12:21:24 -0800

    Fix import and small keyword error.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/layer/input/mapping/TowerHangMapping.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/layer/input/mapping/TowerHangMapping.java
@@ -3,13 +3,14 @@ package org.firstinspires.ftc.teamcode.layer.input.mapping;
 import org.firstinspires.ftc.teamcode.layer.AbstractFunctionLayer;
 import org.firstinspires.ftc.teamcode.layer.LayerSetupInfo;
 import org.firstinspires.ftc.teamcode.task.GamepadInputTask;
+import org.firstinspires.ftc.teamcode.task.Task;
 import org.firstinspires.ftc.teamcode.task.TowerHangTask;  // Either change this for TowerHangTeleopTask or change TowerHangTask to work with teleop.
 import org.firstinspires.ftc.teamcode.task.UnsupportedTaskException;
 
 /**
  * Button mappings for hanging the robot using tower.
  */
-public final class TowerHangMapping implements AbstractFunctionLayer {
+public final class TowerHangMapping extends AbstractFunctionLayer {
     /**
      * Constructor for TowerHangMapping.
      */
@@ -24,6 +25,7 @@ public final class TowerHangMapping implements AbstractFunctionLayer {
             GamepadInputTask castedTask = (GamepadInputTask) task;
             boolean hang = castedTask.gamepad0.buttons.y;
             boolean unhang = castedTask.gamepad0.buttons.a;
+            // Below bit is wrong so it will throw an exception if compiled. Need to fix later.
             return new TowerHangTask((hang ? 1 : 0) - (unhang ? 1 : 0));
         } else {
             throw new UnsupportedTaskException(this, task);
