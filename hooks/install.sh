46c48be7f83d79eadd8b9505d20c884f790cdeaa
Eddy Meals <edimea211@gmail.com>
2024-12-25 23:56:11 -0800

    Run fetch-pack during pre-push
    Run fetch-pack on remote so we can use the initial value of robj as the
    base for diff-index even if robj doesn't exist locally.
    Add a bunch of comments to the pre-push script.
    Add --clobber option to hooks/install.sh.

--- hooks/install.sh
+++ hooks/install.sh
@@ -3,7 +3,17 @@ if test -d hooks
 then
   cd hooks
 fi
-ts=`date +%s`
+case $1 in
+  -c|--clobber)
+    ;;
+  '')
+    ts=`date +%s`
+    ;;
+  *)
+    echo Unrecognized argument $1. >2
+    exit 1
+    ;;
+esac
 for hook in *
 do
   case $hook in
@@ -11,7 +21,10 @@ do
       ;;
     *)
       dst=../.git/hooks/$hook
-      mv $dst $dst.old-$ts 2>/dev/null
+      if test $ts
+      then
+        mv $dst $dst.old-$ts 2>/dev/null
+      fi
       cp $hook $dst
       chmod a+x $dst
       ;;
