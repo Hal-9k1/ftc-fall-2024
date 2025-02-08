fe55b2131ab76aaf39924b34a9de88c21dbff3f7
Ziyao Yuan <zyuanboost2026@gmail.com>
2025-02-08 00:35:44 -0800

    Add some fields for calculatePath function.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/layer/pathfinding/PathfindingLayer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/layer/pathfinding/PathfindingLayer.java
@@ -4,6 +4,7 @@ import org.firstinspires.ftc.teamcode.layer.Layer;
 import org.firstinspires.ftc.teamcode.layer.LayerSetupInfo;
 import org.firstinspires.ftc.teamcode.localization.Mat3;
 import org.firstinspires.ftc.teamcode.localization.Vec2;
+import org.firstinspires.ftc.teamcode.localization.Vec3;
 import org.firstinspires.ftc.teamcode.task.Task;
 
 import java.util.Iterator;
@@ -46,7 +47,27 @@ public class PathfindingLayer implements Layer {
     private double evaluateSpeed(Trajectory t) {
 
     }
+    private void calculatePath() {
+        // Two trajectory fields, one stores current, another stores best. We can probably reassign them during the loop later.
+        Trajectory currentTrajectory;
+        Trajectory bestTrajectory;
+        
+        // Creates two Vec3 to represent the top right upper corner of the rectangular search space.
+        // Another to represent the bottom left lower corner of the rectangular search space.
+        // I don't understand why we need z for this.
+        Vec3 topRightUpper = new Vec3(placeholderX, placeholderY, placeholderZ);
+        Vec3 bottomLeftLower = new Vec3(placeholderX2, placeholderY2, placeholderZ2);
 
+        // Using those two points, we can find the x, y, and z lengths of the rectangular prism.
+        double lengthX = topRightUpper.getX() - bottomLeftLower.getX();
+        double lengthY = topRightUpper.getY() - bottomLeftLower.getY();
+        double lengthZ = topRightUpper.getZ() - bottomLeftLower.getZ();
+
+        // Constant for only searching fixed distanced points in the search space.
+        final int fixedDistance = placeholder;
+        
+        // Implement for loop or something.
+    }
     private boolean checkDynamicWindow(Trajectory t) {
         // hit something?
         // axial and lateral and yaw are possible?
