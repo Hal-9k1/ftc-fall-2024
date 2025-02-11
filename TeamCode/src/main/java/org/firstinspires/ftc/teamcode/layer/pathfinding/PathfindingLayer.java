f16c41126849a31aa915f0a77d67b8426689e63e
Eddy Meals <edimea211@gmail.com>
2025-02-11 01:58:10 -0800

    Partially add javadocs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/layer/pathfinding/PathfindingLayer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/layer/pathfinding/PathfindingLayer.java
@@ -8,15 +8,34 @@ import org.firstinspires.ftc.teamcode.task.Task;
 
 import java.util.Iterator;
 
+/**
+ * Computes holonomic drive powers to pathfind around obstacles to a goal transform.
+ */
 public class PathfindingLayer implements Layer {
+    /**
+     * The distance in meters and offset in radians the robot's transform may be from the goal
+     * before the robot is considered arrived.
+     */
     private static final double GOAL_COMPLETE_EPSILON = 0.01;
 
+    /**
+     * The goal robot transform in field space.
+     */
     private Mat3 goal;
 
+    /**
+     * The current trajectory the robot should take.
+     */
     private Trajectory currentTrajectory;
 
+    /**
+     * The timestamp in nanoseconds the current trajectory was calculated.
+     */
     private long lastCalcTime;
 
+    /**
+     * Constructs a PathfindingLayer.
+     */
     public PathfindingLayer() { }
 
     @Override
@@ -54,31 +73,83 @@ public class PathfindingLayer implements Layer {
         }
     }
 
+    /**
+     * Computes a comparable score for a trajectory considering three factors.
+     *
+     * This is the objective function the dynamic window approach optimizes.
+     *
+     * @param t - the trajectory to evaluate.
+     * @return A comparable score for the trajectory.
+     */
     private double evaluateTrajectory(Trajectory t) {
 
     }
 
+    /**
+     * Computes a comparable score for a trajectory on the grounds of final angle to target.
+     *
+     * @param t - the trajectory to evaluate.
+     * @return A comparable score for the trajectory which is higher the more directly the robot
+     * would face the goal at the end of the evaluated trajectory.
+     */
     private double evaluateTargetAngle(Trajectory t) {
 
     }
 
+
+    /**
+     * Computes a comparable score for a trajectory on the grounds of minimum clearence to
+     * obstacles.
+     *
+     * @param t - the trajectory to evaluate.
+     * @return A comparable score for the trajectory which is higher the greater the minimum
+     * clearence the robot has to any obstacle at any point during the evaluated trajectory.
+     */
     private double evaluateClearence(Trajectory t) {
 
     }
 
+
+    /**
+     * Computes a comparable score for a trajectory on the grounds of final speed of the robot.
+     *
+     * @param t - the trajectory to evaluate.
+     * @return A comparable score for the trajectory which is higher the greater the robot's
+     * translational velocity at the end of the evaluated trajectory..
+     */
     private double evaluateSpeed(Trajectory t) {
 
     }
 
+    /**
+     * Checks if a trajectory is within the dynamic window.
+     *
+     * During optimization, trajectories not within the dynamic window may be culled from the
+     * search.
+     *
+     * @param t - the trajectory to check.
+     * @return Whether the checked trajectory is achievable by the robot and the trajectory would
+     * not cause the robot to crash into an obstacle.
+     */
     private boolean checkDynamicWindow(Trajectory t) {
         // hit something?
         // axial and lateral and yaw are possible?
     }
 
+    /**
+     * Adds an obstacle to the internal list of obstacles.
+     *
+     * @param obstacle - the obstacle to add.
+     */
     private void addObstacle(Obstacle obstacle) {
 
     }
 
+    /**
+     * A set of accelerations that the robot can take.
+     *
+     * Alternately, a 3D point in the search space of trajectories.
+     */
     private static class Trajectory {
         private double axial;
 
